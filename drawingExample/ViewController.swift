5b74173e6c816576bbb5531607e00c4ca1b7f7a4
Erling Austvoll <shakurou@Erling.local>
2015-03-02 09:10:23 +0000

    Initial Commit

---
+++ drawingExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drawingExample
+//
+//  Created by Erling Austvoll on 2015/03/02.
+//  Copyright (c) 2015年 Kingston University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
